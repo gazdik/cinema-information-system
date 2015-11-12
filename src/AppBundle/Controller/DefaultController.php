ee888a71b71a727559fcd040c7c3d26858f18f9d
Peter Gazdik <xgazdi03@stud.fit.vutbr.cz>
2015-11-12 23:58:25 +0100

    Move actions from Cinema and Programme Controller into DefaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,11 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
-use AppBundle\Entity\Hall;
 use AppBundle\Entity\Projection;
-use AppBundle\Entity\Movie;
-use AppBundle\Entity\Cinema;
+use AppBundle\Entity\Form\SearchProjections;
+use AppBundle\Form\SearchProjectionsForm;
+
 
 class DefaultController extends Controller
 {
@@ -24,7 +23,74 @@ class DefaultController extends Controller
 
         return $this->render('index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-            'projections' => $projections
+            'projections' => $projections,
+        ));
+    }
+
+    /**
+     * @Route("/programme", name="programme")
+     */
+    public function programmeAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // Array for arrays of projections
+        $projections;
+
+        // Create object to store form result
+        $search = new SearchProjections();
+        $form = $this->createForm(new SearchProjectionsForm(), $search);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $projections = $em->getRepository('AppBundle:Projection')
+                ->awesomeFind(
+                    $search->getMovie(),
+                    $search->getCinema(),
+                    $search->getDate(),
+                    $search->getGenre());
+        } else {
+            // // Get projections for 3 days
+            // $begin = new \DateTime();
+            // $end = new \DateTime();
+            // $end->modify('+3 days');
+            // $interval = \DateInterval::createFromDateString('1 day');
+            // $period = new \DatePeriod($begin, $interval, $end);
+            //
+            // foreach ($period as $dt) {
+            //     $result = $em->getRepository('AppBundle:Projection')
+            //         ->findByDateOrdered($dt);
+            //
+            //     array_push($projections, $result);
+            // }
+
+            // Get projections for 1 day
+            $date_to = new \DateTime();
+            $date_to->modify('+1 days');
+            $projections = $em->getRepository('AppBundle:Projection')
+                ->findFromToOrderer(new \DateTime(), $date_to);
+        }
+
+        return $this->render('programme.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'projections' => $projections,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/cinemas", name="cinemas")
+     */
+    public function cinemasAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $cinemas = $em->getRepository('AppBundle:Cinema')->findAll();
+
+        return $this->render('cinemas.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'cinemas' => $cinemas,
         ));
     }
 }
