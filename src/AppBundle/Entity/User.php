9c3d6b16eee5e1978e2cce634b3bfb26c0d98635
Marek Bielik <bielik@zulapost.net>
2015-11-15 20:50:32 +0000

    user changed- username's gone

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -24,6 +25,21 @@ class User extends BaseUser
      */
     private $tickets;
 
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $name;
+    
     public function __construct()
     {
         parent::__construct();
@@ -34,7 +50,31 @@ class User extends BaseUser
       return in_array($role, $this->getRoles());
     }
 
-
+    /**
+     * Sets the email.
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+    	$this->setUsername($email);
+    
+    	return parent::setEmail($email);
+    }
+    
+    /**
+     * Set the canonical email.
+     *
+     * @param string $emailCanonical
+     * @return User
+     */
+    public function setEmailCanonical($emailCanonical)
+    {
+    	$this->setUsernameCanonical($emailCanonical);
+    
+    	return parent::setEmailCanonical($emailCanonical);
+    }
 
     /**
      * Add ticket
@@ -68,5 +108,29 @@ class User extends BaseUser
     public function getTickets()
     {
         return $this->tickets;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 }
