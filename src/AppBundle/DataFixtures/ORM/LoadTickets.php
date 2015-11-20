db8cbec0f4e4e223edb9af28904a33bacfc8c3f0
Marek Bielik <bielik@zulapost.net>
2015-11-20 18:06:02 +0000

    tickets added to data fixtures

--- src/AppBundle/DataFixtures/ORM/LoadTickets.php
+++ src/AppBundle/DataFixtures/ORM/LoadTickets.php
@@ -26,23 +26,16 @@ function randomTimestamp($start_date, $end_date)
 
     // Generate random number using above bounds
     return rand($min, $max);
-
-
 }
 
-//tag just turns off LoadTickets class
-?>
 
 class LoadTickets extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
 {
-
-private $discount = array(
-  "student",
-  "senior",
-  "baby",
-  "employee",
-  "dog",
-);
+    //These users will be the tickets sold to
+    private $userEmails = array(
+        'user@email.com',
+        'blockedUser@email.com',
+    );
 
     /**
      * @var ContainerInterface
@@ -63,45 +56,50 @@ private $discount = array(
     public function load(ObjectManager $manager)
     {
       $em = $this->container->get('doctrine')->getEntityManager('default');
+        $userManager = $this->container->get('fos_user.user_manager');
+        $date = new \DateTime();
 
-      // Get projections and clients from database
-      $repository = $em->getRepository('AppBundle:Projection');
-      $projections = $repository->findAll();
-      $repository = $em->getRepository('AppBundle:Client');
-      $clients = $repository->findAll();
+        // Get projections and clients from database
+        $projections = $em->getRepository('AppBundle:Projection')->findAll();
+
+        //Get price categories
+        $priceCategories = $em->getRepository('AppBundle:PriceCategory')->findAll();
+
+        //Get users
+        $users = array();
+
+        foreach ($this->userEmails as $email) {
+            $users[] = $userManager->findUserByEmail('user@mail.com');
+        }
 
       // Add tickets into database
       for($i = 0; $i < 10000; $i++) {
-        $ticket = new Ticket();
-        $projection = $projections[array_rand($projections)];
-        $seats = $projection->getHall()->getSeats()->getValues();
-        $seat = $seats[array_rand($seats)];
+          $ticket = new Ticket();
+          $projection = $projections[array_rand($projections)];
+          $priceCategory = $priceCategories[array_rand($priceCategories)];
 
-        // Set attributes to ticket
-        if (rand(0,1) == 1) {
-          $ticket->setClient($clients[array_rand($clients)]);
-        }
+          $freeSeats = $em->getRepository('AppBundle:Seat')
+              ->findFreeSeats($projection->getId());
+          $seat = $freeSeats[array_rand($freeSeats)];
 
-        $ticket->setProjection($projection);
-        $ticket->setSeat($seat);
-        $ticket->setPrice(rand(50,150));
-        if (rand(0,1) == 1) {
-          $ticket->setDiscount($this->discount[rand(0,3)]);
-        }
+            // Set attributes to ticket
+            if (rand(0,1) == 1)
+              $ticket->setUser($users[array_rand($users)]);
 
-        // Set dates
-        $date = new \DateTime();
-        $date->setTimestamp(randomTimestamp('2015/10/01', '2015/11/30'));
-        if (rand(0,1) == 1) {
-          $ticket->setBookingDate($date);
-        }
-        $date = new \DateTime();
-        $date->setTimestamp(randomTimestamp('2015/10/01', '2015/11/30'));
-        if (rand(0,1) == 1 || !$ticket->getBookingDate()) {
-          $ticket->setPaymentDate($date);
-        }
+            $ticket->setProjection($projection);
+            $ticket->setSeat($seat);
+            $ticket->setPriceCategory($priceCategory);
+            $ticket->setTicketPrice($priceCategory->getCategoryPrice());
 
-        $manager->persist($ticket);
+            $date->setTimestamp(randomTimestamp('2015/10/01', $projection->getDate()->format('Y-m-d')));
+            $ticket->setBookingDate($date);
+
+            $date->setTimestamp(randomTimestamp($date->format('Y-m-d'), $projection->getDate()->format('Y-m-d')));
+            if (rand(0,1) == 1 || !$ticket->getBookingDate()) {
+              $ticket->setPaymentDate($date);
+            }
+
+            $manager->persist($ticket);
       }
       $manager->flush();
     }
